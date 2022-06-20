3450a8344a50a0b2e56ff17c179dd0eda775f683
steamingc <106516831+steamingc@users.noreply.github.com>
2022-06-20 17:54:43 +0900

    220620 Commit

--- baekjoon/src/ClassHour/star.java
+++ baekjoon/src/ClassHour/star.java
@@ -18,26 +18,26 @@ package ClassHour;
 
 
 //2439 별찍기2
-//import java.util.Scanner;
-//public class star {
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		int num = sc.nextInt();
-//		for(int i=0; i<num; i++) {
-//			
-//			for(int j=num-1; j>i; j--) {
-//				System.out.print(" ");
-//			}
-//			
-//			for(int j=0; j<=i; j++) {
-//				System.out.print("*");
-//			}
-//			
-//			System.out.println();
-//		}
-//		sc.close();
-//	}
-//}
+import java.util.Scanner;
+public class star {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		
+		//이중for문에서 바깥for는 행을 의미
+		//이중for문에서 안쪽for는 열을 의미
+		for(int i=0; i<num; i++) {	
+			for(int j=num-1; j>i; j--) {	//공백을 출력하는 반복문
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {		//별을 출력하는 반복문
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
 
 
 //2440 별찍기3
---
+++ jungol/src/ClassHour/J596.java
@@ -0,0 +1,56 @@
+package ClassHour;
+
+import java.util.Scanner;
+
+//public class J596 {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String s = sc.next();
+//		int n = sc.nextInt();
+//		sc.close();
+//		char[] arr = new char[s.length()];
+//		
+//		for(int i=0; i<s.length(); i++) {
+//			arr[i] = s.charAt(i);
+//		}
+//		
+//		for(int i=1; i<=n; i++) {
+//			System.out.print(arr[s.length()-i]);
+//		}
+//	}
+//}
+
+
+//선생님 풀이
+public class J596 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int inp = sc.nextInt();
+		sc.close();
+		
+		
+		//2.----------------------------
+		StringBuilder sb = new StringBuilder(str);
+		
+		StringBuilder sb2 = sb.reverse();
+		
+		for(int i=0; i<inp; i++) {
+			System.out.print(sb2.charAt(i));
+		}
+		
+		
+		//1.----------------------------
+		//inp보다 문자열 길이가 더 길 때
+//		if(str.length()>=inp) {
+//			for(int i=str.length()-1, cnt=0; cnt<inp; i--, cnt ++) {
+//				System.out.print(str.charAt(i));
+//			}
+//		//inp이 문자열 길이보다 길 때
+//		} else {
+//			for(int i=str.length()-1; i>=0; i--) {
+//				System.out.print(str.charAt(i));
+//			}
+//		}
+	}
+}
