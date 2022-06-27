f47b1b1ab593f4ae6306e60802a614b9eafcf0ae
steamingc <106516831+steamingc@users.noreply.github.com>
2022-06-27 17:56:22 +0900

    220627 Commit

---
+++ jungol/src/Alone/반복제어문2.java
@@ -0,0 +1,27 @@
+package Alone;
+
+
+//541
+//import java.util.Scanner;
+//public class 반복제어문2 {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String s = sc.next();
+//		
+//		for(int i=0; i<20; i++) {
+//			System.out.print(s);
+//		}
+//		
+//		sc.close();
+//	}
+//}
+
+
+//542
+//public class 반복제어문2 {
+//	public static void main(String[] args) {
+//		for(int i=10; i<=20; i++) {
+//			System.out.print(i+" ");
+//		}
+//	}
+//}
